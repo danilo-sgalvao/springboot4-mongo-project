9c1d26bfae6a75d6042a421b3995cfe6bcd9b6af
danilo-sgalvao <danilo.galvao@discente.ufg.br>
2026-08-18 15:22:21 -0300

    delecao de usuario com DELETE

--- src/main/java/com/danilogalvao/workshopmongo/resources/UserResource.java
+++ src/main/java/com/danilogalvao/workshopmongo/resources/UserResource.java
@@ -44,4 +44,10 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @RequestMapping(value="/{id}", method= RequestMethod.DELETE)
+    public ResponseEntity <Void> delete(@PathVariable String id) {
+        userServ.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/danilogalvao/workshopmongo/services/UserService.java
+++ src/main/java/com/danilogalvao/workshopmongo/services/UserService.java
@@ -31,7 +31,14 @@ public class UserService {
         return userRepo.insert(obj);
     }
 
+    public void delete(String id){
+        findById(id);
+        userRepo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
+
+
 }
