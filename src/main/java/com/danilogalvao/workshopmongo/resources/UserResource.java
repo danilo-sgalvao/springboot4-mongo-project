3523f25d8984a388e17f2dda7c49cd139f47f731
danilo-sgalvao <danilo.galvao@discente.ufg.br>
2026-08-18 15:13:57 -0300

    Insercao de usuario com POST

--- src/main/java/com/danilogalvao/workshopmongo/resources/UserResource.java
+++ src/main/java/com/danilogalvao/workshopmongo/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.danilogalvao.workshopmongo.domain.User;
 import com.danilogalvao.workshopmongo.dto.UserDTO;
 import com.danilogalvao.workshopmongo.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,5 +35,13 @@ public class UserResource {
     public ResponseEntity <UserDTO> findById(@PathVariable String id) {
         User user = userServ.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @RequestMapping(method= RequestMethod.POST)
+    public ResponseEntity <Void> insert(@RequestBody UserDTO objDto) {
+        User user = userServ.fromDTO(objDto);
+        user = userServ.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/danilogalvao/workshopmongo/services/UserService.java
+++ src/main/java/com/danilogalvao/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.danilogalvao.workshopmongo.services;
 
 import com.danilogalvao.workshopmongo.domain.User;
+import com.danilogalvao.workshopmongo.dto.UserDTO;
 import com.danilogalvao.workshopmongo.repository.UserRepository;
 import com.danilogalvao.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,13 @@ public class UserService {
     public  User findById(String id){
         Optional<User> user = userRepo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+    }
+
+    public User insert(User obj) {
+        return userRepo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
